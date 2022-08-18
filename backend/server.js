b46ee3fc35b9d808657ccb5bb1feb01770194691
Dimterion <dimterion59@gmail.com>
2022-08-18 12:25:46 +0200

    Added backend for the tickets functionality.

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' })
@@ -19,6 +19,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 app.use(errorHandler)
 
