3f15b157354193ef347289844f782b62a3e6e21c
Dimterion <dimterion59@gmail.com>
2022-08-20 14:18:27 +0200

    server.js adjustment

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === 'production') {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+  app.get('*', (_, res) => res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html')))
 } else {
   app.get('/', (req, res) => {
     res.status(200).json({ message: 'Welcome to the Support Desk API' })
